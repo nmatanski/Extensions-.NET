5ec54ce071565c0025068ab2b51b3f75f2ca85a0
nmatanski <nmatanski95@abv.bg>
2019-07-04 19:08:04 +0300

    SetPrecision extension method of the class/type Float so it's usable on float numbers like in the setprecision(prec) in C++

---
+++ SetPrecisionExtension/SetPrecisionExtension/SetPrecisionExtension/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SetPrecisionExtensions;
+
+namespace Example
+{
+    internal static class Examples
+    {
+        static void Main(string[] args)
+        {
+            float number = 3.574857f;
+            Console.WriteLine(number.SetPrecision(0));
+            Console.WriteLine(number.SetPrecision(2));
+            Console.WriteLine(number.SetPrecision(8));
+            Console.WriteLine(5.545454576586758675687f.SetPrecision(2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SetPrecisionExtension/SetPrecisionExtension/SetPrecisionExtension/SetPrecisionExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace SetPrecisionExtensions
+{
+    public static class SetPrecisionExtension
+    {
+        public static string SetPrecision(this float num, int precision)
+        {
+            try
+            {
+                if (precision == 0)
+                {
+                    return ((int)Math.Round(num)).ToString();
+                }
+                string numberString = num.ToString();
+                int lastIndex = numberString.IndexOf(".") + precision;
+                if (lastIndex + 1 >= numberString.Length)
+                {
+                    return numberString;
+                }
+                if (Convert.ToInt32(numberString[lastIndex + 1]) > 4)
+                {
+                    var str = new StringBuilder(numberString);
+                    str[lastIndex] = char.Parse((Convert.ToInt32(numberString[lastIndex].ToString()) + 1).ToString());
+                    numberString = str.ToString();
+                }
+                return numberString.Substring(0, numberString.IndexOf(".") + precision + 1);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The precision is invalid!");
+                return num.ToString();
+            }
+        }
+    }
+}
